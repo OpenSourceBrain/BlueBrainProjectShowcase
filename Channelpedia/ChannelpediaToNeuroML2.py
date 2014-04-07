959fa718836d19de89b91f848ab944d65fe6d70c
Padraig Gleeson <p.gleeson@gmail.com>
2014-04-07 16:17:31 +0100

    More structured metadata in generated NML2 files
    Uses classes at https://github.com/OpenSourceBrain/OSB_API

--- Channelpedia/ChannelpediaToNeuroML2.py
+++ Channelpedia/ChannelpediaToNeuroML2.py
@@ -5,30 +5,16 @@ import lems.api as lems
 
 import xml.etree.ElementTree as ET
 
-import re
+# This is available at https://github.com/OpenSourceBrain/OSB_API
+import osb.metadata
+import osb.resources
 
 import sys
 
-def update_metadata(element, metadata, about_id, urn):
-    
-    template = '\n            <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"  xmlns:bqmodel="http://biomodels.net/model-qualifiers/" xmlns:bqbiol="http://biomodels.net/biology-qualifiers/">\n'+\
-    '                <rdf:Description rdf:about="%s">\n'+\
-    '                    <bqmodel:isDescribedBy>\n'+\
-    '                        <rdf:Bag>\n'+\
-    '                            <rdf:li rdf:resource="%s"/>\n'+\
-    '                        </rdf:Bag>\n'+\
-    '                    </bqmodel:isDescribedBy>\n'+\
-    '                </rdf:Description>\n'+\
-    '            </rdf:RDF>\n'
-    
-    annotation = neuroml.Annotation()
-    element.annotation = annotation
-    metadata = metadata + template%(about_id, urn)
-    return metadata
-
 def channelpedia_xml_to_neuroml2(cpd_xml, nml2_file_name):
     
-    print('Converting Channelpedia XML to NeuroML2')
+    info = 'Automatic conversion of Channelpedia XML file to NeuroML2'
+    print(info)
     
     root = ET.fromstring(cpd_xml)
         
@@ -36,7 +22,8 @@ def channelpedia_xml_to_neuroml2(cpd_xml, nml2_file_name):
     
     doc = neuroml.NeuroMLDocument()
     
-    metadata = ''
+    metadata = osb.metadata.RDF(info)
+    
     
     ion = root.findall('Ion')[0]
     chan = neuroml.IonChannelHH(id=channel_id,
@@ -45,9 +32,53 @@ def channelpedia_xml_to_neuroml2(cpd_xml, nml2_file_name):
                           notes="This is an automated conversion to NeuroML 2 of an ion channel model from Channelpedia. "
                           "\nThe original model can be found at: http://channelpedia.epfl.ch/ionchannels/%s"%root.attrib['ID'])
     
+    chan.annotation = neuroml.Annotation()
+    
+    model_url_template = 'http://channelpedia.epfl.ch/ionchannels/%s/hhmodels/%s.xml'
+    desc = osb.metadata.Description(channel_id)
+    metadata.descriptions.append(desc)
+    osb.metadata.add_simple_qualifier(desc, \
+                                      'bqmodel', \
+                                      'isDerivedFrom', \
+                                      model_url_template%(root.attrib['ID'], root.attrib['ModelID']), \
+                                      "Channelpedia channel ID: %s, ModelID: %s; direct link to original XML model" % (root.attrib['ID'], root.attrib['ModelID']))
+    
+    channel_url_template = 'http://channelpedia.epfl.ch/ionchannels/%s'
+    osb.metadata.add_simple_qualifier(desc, \
+                                      'bqmodel', \
+                                      'isDescribedBy', \
+                                      channel_url_template%(root.attrib['ID']), \
+                                      "Channelpedia channel ID: %s; link to main page for channel" % (root.attrib['ID']))
+    
     for reference in root.findall('Reference'):
         pmid = reference.attrib['PubmedID']
-        metadata = update_metadata(chan, metadata, channel_id, "http://identifiers.org/pubmed/%s"%pmid)
+        #metadata = update_metadata(chan, metadata, channel_id, "http://identifiers.org/pubmed/%s"%pmid)
+
+        pubmed_url_template = "http://identifiers.org/pubmed/%s"
+        osb.metadata.add_simple_qualifier(desc, \
+                                          'bqmodel', \
+                                          'isDescribedBy', \
+                                          pubmed_url_template%(pmid), \
+                                          "PubMed ID: %s is referenced in original XML" % (pmid))
+            
+    for environment in root.findall('Environment'):
+        for animal in environment.findall('Animal'):
+
+            species = animal.attrib['Name'].lower()
+
+            if species:
+                if osb.resources.KNOWN_SPECIES.has_key(species):
+                    known_id = osb.resources.KNOWN_SPECIES[species]
+                    osb.metadata.add_simple_qualifier(desc, \
+                                                      'bqbiol', \
+                                                      'hasTaxon', \
+                                                      'http://identifiers.org/taxonomy/%s'%known_id, \
+                                                      "Known species: %s; taxonomy id: %s" % (species, known_id))
+                else:
+                    print("Unknown species: %s"%species)
+
+        
+
                           
     comp_types = {}
     for gate in root.findall('Gates'):
@@ -154,16 +185,15 @@ def channelpedia_xml_to_neuroml2(cpd_xml, nml2_file_name):
         nml2_file = open(nml2_file_name, 'w')
         nml2_file.write(new_contents)
         nml2_file.close()
-        
-    if len(metadata)>0:
-        print("Inserting metadata: %s"%metadata)
-        nml2_file = open(nml2_file_name, 'r')
-        orig = nml2_file.read()
-        new_contents = orig.replace("<annotation/>", "\n        <annotation>%s        </annotation>\n"%metadata)
-        nml2_file.close()
-        nml2_file = open(nml2_file_name, 'w')
-        nml2_file.write(new_contents)
-        nml2_file.close()
+
+    print("Inserting metadata...")
+    nml2_file = open(nml2_file_name, 'r')
+    orig = nml2_file.read()
+    new_contents = orig.replace("<annotation/>", "\n        <annotation>\n%s        </annotation>\n"%metadata.to_xml("            "))
+    nml2_file.close()
+    nml2_file = open(nml2_file_name, 'w')
+    nml2_file.write(new_contents)
+    nml2_file.close()
         
 
     ###### Validate the NeuroML ######    
