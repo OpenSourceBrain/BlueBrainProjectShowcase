b95e9ab6881858f079cf0a745d3ee191d24cc8c4
Rayner Lucas <github@magic-cookie.co.uk>
2015-01-02 21:48:28 +0000

    Added peak absolute value plot (useful for Ca channel)

--- Channelpedia/iv_analyse.py
+++ Channelpedia/iv_analyse.py
@@ -22,8 +22,9 @@ def main():
     v_arr = []
     ih = []
     il = []
+    ip = []
 
-    print "voltage (mV)   i_lowest (A)   i_highest (A)"
+    print "voltage (mV)   i_lowest (A)   i_highest (A)   i_peak (A)"
 
     # Find highest, lowest, and steady-state current from each file
     voltages = file_dict.keys()
@@ -31,7 +32,7 @@ def main():
     for voltage in voltages:
         i_highest = None
         i_lowest = 10000
-        i_steady = None
+        i_peak = None
 
         i_file = open(file_dict[voltage], "r")
 
@@ -46,21 +47,39 @@ def main():
             if i > i_highest:
                 i_highest = i;
 
-        print "{0:>12}{1:>15E}{2:>15E}".format(voltage, i_lowest, i_highest)
+        if abs(i_lowest) > abs(i_highest):
+            i_peak = i_lowest
+        else:
+            i_peak = i_highest
+
+        print "{0:>12}{1:>15E}{2:>15E}{3:>15E}".format(voltage, i_lowest, i_highest, i_peak)
 
         v_arr.append(float(voltage));
         ih.append(float(i_highest));
         il.append(float(i_lowest));
+        ip.append(float(i_peak));
 
     x=np.array(v_arr)
     y=np.array(ih)
     plt.figure(1)
-    plt.subplot(211)
     plt.plot(x,y)
+    plt.title("Highest current")
+    plt.xlabel("voltage (mV)")
+    plt.ylabel("current (A)")
 
     y=np.array(il)
-    plt.subplot(212)
+    plt.figure(2)
     plt.plot(x,y)
+    plt.title("Lowest current")
+    plt.xlabel("voltage (mV)")
+    plt.ylabel("current (A)")
+
+    y=np.array(ip)
+    plt.figure(3)
+    plt.plot(x,y)
+    plt.title("Peak absolute current")
+    plt.xlabel("voltage (mV)")
+    plt.ylabel("current (A)")
 
     plt.show()
 
