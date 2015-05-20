92ba8c96f3b61b345fe4e140c72a734e8246ee20
Padraig Gleeson <p.gleeson@gmail.com>
2015-05-20 14:23:35 +0100

    Retested with latest libNeuroML & updated appropriately

--- Channelpedia/ChannelpediaToNeuroML2.py
+++ Channelpedia/ChannelpediaToNeuroML2.py
@@ -105,14 +105,12 @@ def channelpedia_xml_to_neuroml2(cpd_xml, nml2_file_name, unknowns=""):
         
         eq_type = gate.attrib['EqType']
         gate_name = gate.attrib['Name']
-        target = None
+        target = chan.gates
         
         if eq_type == '1':
-            g = neuroml.GateHHTauInf(id=gate_name,instances=int(float(gate.attrib['Power'])))
-            target = chan.gate_hh_tau_infs
+            g = neuroml.GateHHUndetermined(id=gate_name, type='gateHHtauInf', instances=int(float(gate.attrib['Power'])))
         elif eq_type == '2':
-            g = neuroml.GateHHRates(id=gate_name,instances=int(float(gate.attrib['Power'])))
-            target = chan.gate_hh_rates
+            g = neuroml.GateHHUndetermined(id=gate_name, type='gateHHrates', instances=int(float(gate.attrib['Power'])))
         
         for inf in gate.findall('Inf_Alpha'):
             equation = check_equation(inf.findall('Equation')[0].text)
