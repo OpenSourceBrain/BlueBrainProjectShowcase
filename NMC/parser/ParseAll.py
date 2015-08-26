f4c3672ae1cbd26c2486dd57703c60f767cb392f
Padraig Gleeson <p.gleeson@gmail.com>
2015-08-26 20:09:57 +0100

    Now all channels except Ih included & close match NRN->NML2

--- NMC/parser/ParseAll.py
+++ NMC/parser/ParseAll.py
@@ -201,9 +201,7 @@ wopen()
                       "SKv3_1", "SK_E2", "CaDynamics_E2", "Nap_Et2", "Im",
                       "K_Tst", "NaTa_t", "K_Pst", "NaTs2_t"]
                       
-        ignore_chans=['Ih', 'Ca', 
-                      "SKv3_1", "SK_E2", "Im",
-                      "NaTa_t" ]
+        ignore_chans=['Ih']
             
         bp, incl_chans = get_biophysical_properties(cell_info['e-type'], 
                                                     ignore_chans=ignore_chans,
