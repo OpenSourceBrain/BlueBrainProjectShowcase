5dae1b7a4f47441f31e95c38e2a348d190cd07b3
Padraig Gleeson <p.gleeson@gmail.com>
2015-10-12 14:23:32 +0100

    Example network

---
+++ NMC/NeuroML2/Examples.py
@@ -0,0 +1,71 @@
+
+from pyneuroml import pynml
+from neuroml import *
+import os
+import math
+
+
+max_per_layer = 100
+layers = ['L1','L23', 'L4', 'L5', 'L6']
+
+radius = 210.0
+t1=-165.0
+t2=-149.0
+t3=-353.0
+t4=-190.0
+t5=-526.0
+t6=-700.0
+ys = {}
+
+ys['L1']=[0,t1]
+ys['L23']=[t1,t1+t2+t3]
+ys['L4']=[t1+t2+t3,t1+t2+t3+t4]
+ys['L5']=[t1+t2+t3+t4,t1+t2+t3+t4+t5]
+ys['L6']=[t1+t2+t3+t4+t5,t1+t2+t3+t4+t5+t6]
+
+
+
+net_ref = "CellTypesPerLayer"
+net_doc = NeuroMLDocument(id=net_ref)
+
+net = Network(id=net_ref)
+net_doc.networks.append(net)
+
+count = 0
+
+cells = {}
+cells['L1'] = ['bNAC219_L1_DAC_a9ae5cbbf5_0_0.cell.nml', 
+               'cNAC187_L1_HAC_f8c9772d9d_0_0.cell.nml']
+cells['L23'] = ['cSTUT189_L23_LBC_e6e8f83407_0_0.cell.nml']
+cells['L4'] = []
+cells['L5'] = ['cADpyr232_L5_TTPC1_0fb1ca4724_0_0.cell.nml']
+cells['L6'] = []
+
+
+for layer in ['L1','L23','L4','L5','L6']:
+    for cell in cells[layer]:
+
+        net_doc.includes.append(IncludeType(cell))
+        bbp_ref = cell.split('.')[0]
+
+        pop = Population(id="Pop_%s"%bbp_ref, component=bbp_ref, type="populationList")
+
+        net.populations.append(pop)
+
+        inst = Instance(id="0")
+        pop.instances.append(inst)
+        
+	X=count*300
+	Z=0
+
+	Y = ys[layer][1] + 0.5*(ys[layer][0] - ys[layer][1])
+        inst.location = Location(x=X, y=Y, z=Z)
+        
+        count+=1
+
+net_file = '%s.net.nml'%(net_ref)
+writers.NeuroMLWriter.write(net_doc, net_file)
+
+print("Written network with %i cells in network to: %s"%(count,net_file))
+
+pynml.nml2_to_svg(net_file)
